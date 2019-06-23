77e7a5776cf6b87e47b98f2a6dcbff008ae81896
ThanhVu Nguyen <nguyenthanhvuh@gmail.com>
2019-06-23 09:02:31 -0500

    add timeout to benchmark

--- src/benchmark.sh
+++ src/benchmark.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 FILES=../tests/nla/*.java
 END=1
+TIMEOUT=600   #10 mins
 for f in $FILES
 do
     for i in $(seq 1 $END); do
-	echo "$i. Processing $f file..."
+	echo "$i. analyzing $f"
 	# take action on each file. $f store current file name
-	sage -python -O dig.py $f -log 3 -octmaxv 20 -seed $i
+	timeout $TIMEOUT sage -python -O dig.py $f -log 3 -octmaxv 20 -seed $i
      done
 done
