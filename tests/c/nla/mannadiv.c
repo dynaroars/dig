d650cc81665dea915752c216fbfe6813dfac6cac
ThanhVu (Vu) Nguyen <nguyenthanhvuh@gmail.com>
2020-01-04 19:27:37 -0600

    updates

---
+++ tests/c/nla/mannadiv.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void vassume(int b){}
+void vtrace1(int q, int a, int b, int x, int y){}  
+void vtrace2(int q, int a, int x, int y){}
+
+int mainQ(int x, int y){
+     
+    vassume(x >= 0);
+    vassume(y >= 1);   //can also be y!=0, but CIVL will give disj
+     
+    int q, a, b; 
+    q = 0;
+    a = 0;
+    b = x;
+
+    while(1) {
+	//assert(q* y + a + b == x);
+	vtrace1(q, a, b, x, y);
+	if(!(b != 0)) break;
+	  
+	if (a + 1 == y) {
+	    q = q + 1;
+	    a = 0;
+	    b = b - 1;
+	}
+	else {
+	    a = a + 1;
+	    b = b - 1;
+	}
+    }
+    vtrace2(q, a, x, y); 
+    //assert(q == x / y);
+    return q;
+}
+
+void main(int argc, char **argv){
+    mainQ(atoi(argv[1]), atoi(argv[2]));
+}
+
---
+++ tests/c/nla/prodbin.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void vassume(int b){}
+void vtrace1(int a, int b, int x, int y, int z){}
+void vtrace2(int a, int b, int x, int z){}
+
+int mainQ(int a, int b){
+    vassume(a>=0);
+    vassume(b>=0);
+
+    int x,y,z;
+    x = a;
+    y = b;
+    z = 0;
+
+    while(1) { 
+	//assert(z+x*y==a*b);
+	//%%%traces: int a, int b, int x, int y, int z
+	vtrace1(a, b, x, y, z);
+	if(!(y!=0)) break;
+	  
+	if (y%2 ==1 ){
+	    z = z+x;
+	    y = y-1;
+	}
+	x = 2*x;
+	y = y/2;
+
+    }
+    vtrace2(a, b, x, z);
+    //assert(z == a*b);
+    return z; 
+
+}
+
+
+void main(int argc, char **argv){
+    mainQ(atoi(argv[1]), atoi(argv[2]));
+}
+
