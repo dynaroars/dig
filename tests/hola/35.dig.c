2cf3b81e4b785630c2caa92881088ec99c247b30
Didier Ishimwe <ishimwed@huskers.unl.edu>
2020-02-14 09:58:41 -0600

    Updated 35.dig.c

--- tests/hola/35.dig.c
+++ tests/hola/35.dig.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 
-void mainQ(int n) { 
+void vtrace(int i, int j){}
+
+void mainQ(int n) {
   int x = 0;
   while (x < n) {
     x++;
   }
 
   //%%%traces: int x, int n
+  vtrace(x, n);
   //if (n > 0) assert(x == n);
 }
 
-int main(int argc, char *argv[]) {
+void main(int argc, char *argv[]) {
   mainQ(atoi(argv[1]));
-  return 0;
 }
