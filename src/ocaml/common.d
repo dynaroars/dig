76a133b4f464fd579c34da308202ba08067800ba
ThanhVu (Vu) Nguyen <nguyenthanhvuh@gmail.com>
2020-02-22 20:53:24 -0600

    rm binary files

--- src/ocaml/common.d
+++
@@ -1,6 +0,0 @@
-common.cmo : /home/SHARED/Devel/CIL/cil/_build/src/ocamlutil/pretty.cmi \
-    /home/SHARED/Devel/CIL/cil/_build/src/ocamlutil/errormsg.cmi \
-    /home/SHARED/Devel/CIL/cil/_build/src/cil.cmi
-common.cmx : /home/SHARED/Devel/CIL/cil/_build/src/ocamlutil/pretty.cmx \
-    /home/SHARED/Devel/CIL/cil/_build/src/ocamlutil/errormsg.cmx \
-    /home/SHARED/Devel/CIL/cil/_build/src/cil.cmx
