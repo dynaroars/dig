703caa58d9ad190710bb65806969aa9589bb08bf
ThanhVu Nguyen <nguyenthanhvuh@gmail.com>
2020-03-23 12:17:47 -0500

    add working Freir1_Int file

---
+++ tests/nla/Freire1_Int.java
@@ -0,0 +1,24 @@
+public class Freire1_Int {
+    public static void vtrace1(float a, float x, float r){}
+    public static void vtrace2(float a, float x, float r){}
+    
+    public static void main (String[] args) {
+    }
+
+    public static void mainQ(int x) {
+	int a = x * 2;
+	int r = 0;
+
+	while(true){
+	    vtrace1(a,x,r);
+	    //assert((float)a == 2*x + r*r - r); 
+	    if (!(x>r)) break;
+	    x = x - r;
+	    r = r + 1;
+	}
+
+	vtrace2(a,x,r);
+	//assert((float)a == 2*x + r*r - r); 
+	//return r;	  
+    }
+}
