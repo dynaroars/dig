d41e3f3ab079c389fd5f352cea55e94b2ed28387
ThanhVu Nguyen <nguyenthanhvuh@gmail.com>
2020-03-27 20:31:58 -0500

    clean up analysis

--- src/analysis.py
+++ src/analysis.py
@@ -36,6 +36,22 @@ class Result:
         self.depth_changes = depth_changes
         self.t_time = t_time
 
+    def analyze(self):
+        self.solver_calls_ctr = Counter(
+            str(stat) for stat, is_succ in self.solver_calls)
+
+        self.change_stats_ctr = Counter(
+            self.get_inv_typ(inv)
+            for inv, stat0, depth0, stat1, depth1 in self.depth_changes)
+
+        self.change_typs_ctr = Counter(
+            self.get_change(stat0, stat1)
+            for inv, stat0, depth0, stat1, depth1 in self.depth_changes)
+
+        self.change_depths_ctr = Counter(
+            self.get_change(depth0, depth1)
+            for inv, stat0, depth0, stat1, depth1 in self.depth_changes)
+
     def save(self, todir):
         assert todir.is_dir(), todir
         CM.vsave(todir / self.resultfile, self)
@@ -46,79 +62,18 @@ class Result:
 
         return CM.vload(fromdir / cls.resultfile)
 
-    @property
-    def nlocs(self):
-        return len(self.dinvs)
+    @classmethod
+    def get_inv_typ(cls, inv):
+        try:
+            return inv.__class__.__name__
+        except AttributeError:
+            assert inv is None
+            print('W: unknown inv type: {}'.format(inv))
+            return "Unknown"
 
-    @property
-    def ninvs(self):
-        return self.dinvs.siz
-
-    @property
-    def invtyps(self):
-        return self.dinvs.typ_ctr
-
-    # def str_of_dict(self, d):
-    #     return "{} ({})".format(
-    #         sum(d[k] for k in d),
-    #         ', '.join('{} {}'.format(k, d[k]) for k in sorted(d)))
-
-    def analyze_symstates(self):
-
-        solver_calls = Counter(str(stat)
-                               for stat, is_succ in self.solver_calls)
-
-        def get_inv_typ(inv):
-            # assert inv is None or isinstance(inv, Inv), inv
-            try:
-                return inv.__class__.__name__
-            except AttributeError:
-                assert inv is None
-                print('W: unknown inv type: {}'.format(inv))
-                return "Unknown"
-
-        def get_change(x, y):
-            return "{}->{}".format(x, y)
-
-        change_stats = Counter(get_inv_typ(inv)
-                               for inv, stat0, depth0, stat1, depth1 in self.depth_changes)
-        # unsat->sat
-        change_typs = Counter(get_change(stat0, stat1)
-                              for inv, stat0, depth0, stat1, depth1 in self.depth_changes)
-        # 9->10, 10->11
-        change_depths = Counter(get_change(depth0, depth1)
-                                for inv, stat0, depth0, stat1, depth1 in self.depth_changes)
-
-        return solver_calls, change_stats, change_typs, change_depths
-
-        # solver_calls_s = "solver calls {}".format(
-        #     self.str_of_dict(solver_calls))
-        # change_stats_s = "stats {}".format(self.str_of_dict(change_stats))
-        # change_typs_s = "change typs {}".format(self.str_of_dict(change_typs))
-        # change_depths_s = "change depths {}".format(
-        #     self.str_of_dict(change_depths))
-
-        # ss = [solver_calls_s, change_stats_s, change_typs_s, change_depths_s]
-        # return ', '.join(ss)
-
-    # def analyze(self):
-    #     inv_typs = ', '.join('{} {}'.format(self.invtyps[t], t)
-    #                          for t in sorted(self.invtyps))
-
-    #     symstates_s = self.analyze_symstates()
-
-    #     ss = [  # "file {}".format(dig.filename),
-    #         "locs {}".format(len(self.dinvs)),
-    #         "invs {} ({})".format(self.dinvs.siz, inv_typs),
-    #         "traces {}".format(self.dtraces.siz),
-    #         "inps {}".format(len(self.inps) if self.inps else 0),
-    #         "time {:.2f}s".format(self.t_time),
-    #         symstates_s,
-    #         "rand seed {}, test ({}, {})".format(
-    #             self.seed, random.randint(0, 100), sage.all.randint(0, 100))
-    #     ]
-    #     print("***",  ', '.join(s for s in ss if s))
-    #     print(self.dinvs.__str__(print_stat=False))
+    @classmethod
+    def get_change(cls, x, y):
+        return "{}->{}".format(x, y)
 
 
 class Stats:
@@ -148,20 +103,11 @@ class Stats:
         return '{} {} ({})'.format(label, f(dd['siz']), s)
 
     def analyze2(self, f):
-        invtypss = []
-        solver_callss = []
-        change_statss = []
-        change_typss = []
-        change_depthss = []
-        for result in self.results:
-            invtypss.append(result.invtyps)
-            solver_calls, change_stats, change_typs, change_depths = \
-                result.analyze_symstates()
-            solver_callss.append(solver_calls)
-            change_statss.append(change_stats)
-            change_typss.append(change_typs)
-            change_depthss.append(change_depths)
-
+        invtypss = [r.dinvs.typ_ctr for r in self.results]
+        solver_callss = [r.solver_calls_ctr for r in self.results]
+        change_statss = [r.change_stats_ctr for r in self.results]
+        change_typss = [r.change_typs_ctr for r in self.results]
+        change_depthss = [r.change_depths_ctr for r in self.results]
         ss = [
             self.analyze_dicts(invtypss, f, 'invtyps'),
             self.analyze_dicts(solver_callss, f, 'solver calls'),
@@ -174,11 +120,12 @@ class Stats:
 
     def analyze(self, f):
         rs = self.results
+        _ = [r.analyze() for r in rs]
 
         ss = [
             "prog {}".format(self.prog),
             "runs {}".format(len(rs)),
-            "locs {}".format(f(r.nlocs for r in rs)),
+            "locs {}".format(f(len(r.dinvs) for r in rs)),
             "traces {}".format(f(r.dinvs.siz for r in rs)),
             "inps {}".format(
                 f(len(r.inps) if r.inps else 0 for r in rs)),
