f16127c49f52cba3a819ce99d6f6afb8e9bfc22b
ThanhVu (Vu) Nguyen <nguyenthanhvuh@gmail.com>
2020-09-13 19:43:56 -0500

    update StrnCopy.java

--- benchmark/java/mp/ignore/StrnCopy.java
+++ benchmark/java/mp/ignore/StrnCopy.java
@@ -1,21 +1,25 @@
 public class StrnCopy {
-    public static void vtrace1(int q, int r, int a, int b, int x, int y){}
-    public static void vtrace2(int q, int r, int a, int b, int x, int y){}
-    public static void vtrace3(int x, int y, int q, int r){}
-
+    public static void vtrace_post(int n){}
     public static void main (String[] args) {}
      
+	public static int mainQ(int n, int srcsiz, int destsiz){
+		int src, i, c;
 
-    public static int mainQ_cohendiv(int n, int siz) {
+		src = srcsiz; // Comment this line out will make it work
 
-	
-	
-	assert(!(n <= ls) || ld >= n);
-	assert(!(n > ls) || ld == ls);
+		i = 0;
+		while(i < n){
+			if (i == srcsiz) {
+				c = 0;
+			} else {
+				c = 32;
+			}
+		    i = i + 1;
+		}
 
-	vtrace1(n,siz,ls,ld);
-
-    }
+		vtrace_post(n);
+		return 0;
+	}
 }
 
 // //Orig C code
@@ -77,68 +81,68 @@ public class StrnCopy {
     
 
 
-/* package whatever; // don't place package name! */
-/*
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+// /* package whatever; // don't place package name! */
+// /*
+// import java.util.*;
+// import java.lang.*;
+// import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
-{
-	static int getchar(final int str, final int idx, final int siz) {
-		assert 0 <= idx && idx < siz && 0 <= str & str <= siz;
-		return idx == str ? 0 : ' ';
-	}
-	static int setchar(final int str, final int idx, final int siz, final int c) {
-		assert 0 <= idx && idx < siz && 0 <= str & str <= siz;
-		if (c == 0) {
-			if (idx < str) return idx;
-		} else if(str == idx) {
-			return siz;
-		}
-		return str; // no change
-	}
-	static void strncopy(final int n, final int siz, final int srcsiz, final int destsiz){
-		assert (1 <= siz);
-		assert (0 <= n);
-		assert (n <= siz-1);
+// /* Name of the class has to be "Main" only if the class is public. */
+// class Ideone
+// {
+// 	static int getchar(int str, int idx, int siz) {
+// 		assert 0 <= idx && idx < siz && 0 <= str & str <= siz;
+// 		return idx == str ? 0 : ' ';
+// 	}
+// 	static int setchar(int str, int idx, int siz, int c) {
+// 		assert 0 <= idx && idx < siz && 0 <= str & str <= siz;
+// 		if (c == 0) {
+// 			if (idx < str) return idx;
+// 		} else if(str == idx) {
+// 			return siz;
+// 		}
+// 		return str; // no change
+// 	}
+// 	static void strncopy(int n, int siz, int srcsiz, int destsiz){
+// 		assert (1 <= siz);
+// 		assert (0 <= n);
+// 		assert (n <= siz-1);
 		
-		int src = srcsiz;
-		int dst = destsiz;
+// 		int src = srcsiz;
+// 		int dst = destsiz;
 		
-		int i = 0;
-		while(i < n && getchar(src, i, siz) != 0 /*src[i] != '\0'*/){
-		    //dst[i] = src[i];
-		    dst = setchar(dst, i, siz, getchar(src, i, siz));
-		    i = i + 1;
-		}
-		while(i < n){
-		    //dst[i] = '\0';
-		    dst = setchar(dst, i, siz, 0);
-		    i = i + 1;
-		}
+// 		int i = 0;
+// 		while(i < n && getchar(src, i, siz) != 0 /*src[i] != '\0'*/){
+// 		    //dst[i] = src[i];
+// 		    dst = setchar(dst, i, siz, getchar(src, i, siz));
+// 		    i = i + 1;
+// 		}
+// 		while(i < n){
+// 		    //dst[i] = '\0';
+// 		    dst = setchar(dst, i, siz, 0);
+// 		    i = i + 1;
+// 		}
 		
-		int ls = mylen(n,src,siz);
-		int ld = mylen(n,dst,siz);
-		System.out.printf("l_post: n s d\n");
-		System.out.printf("l_post: %d %d %d\n", n, ls, ld);
-		//inv
-		assert(!(n <= ls) || ld >= n);
-		assert(!(n > ls) || ld == ls);
-	}
-	static int mylen(final int k, final int str, final int siz){
-		assert 0 <= str & str <= siz;
-		if (str == siz) {
-			return siz-1;
-		}
-		return str;
-	}
+// 		int ls = mylen(n,src,siz);
+// 		int ld = mylen(n,dst,siz);
+// 		System.out.printf("l_post: n s d\n");
+// 		System.out.printf("l_post: %d %d %d\n", n, ls, ld);
+// 		//inv
+// 		assert(!(n <= ls) || ld >= n);
+// 		assert(!(n > ls) || ld == ls);
+// 	}
+// 	static int mylen(int k, int str, int siz){
+// 		assert 0 <= str & str <= siz;
+// 		if (str == siz) {
+// 			return siz-1;
+// 		}
+// 		return str;
+// 	}
 	
 	
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		strncopy(4, 10, 7, 1);
-	}
-}
-*/
+// 	public static void main (String[] args) throws java.lang.Exception
+// 	{
+// 		strncopy(4, 10, 7, 1);
+// 	}
+// }
+// */
