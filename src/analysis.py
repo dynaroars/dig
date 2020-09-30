d0b7d5775e6f874dc18481894387d045efaaa9a2
KH <kh@kh>
2020-09-30 12:58:57 -0500

    ana 20

--- src/analysis.py
+++ src/analysis.py
@@ -143,6 +143,7 @@ class AResult(Result):
             for x in self.check_depthchanges:
                 d = x.d2
                 val = ddepths.get(d, 0)
+                assert str(x.v2) in set(['sat', 'unsat', 'unknown'])
                 if str(x.v2) == 'sat': # disproved
                     if d > mind:
                         ddepths[d] = val - 1
@@ -402,8 +403,11 @@ class Results:
         elif SEL == 20:
             agg_check_invs_at_depth = [r.check_invs_at_depth for r in rs]
             _, agg_check_invs_at_depth = self.analyze_dicts(agg_check_invs_at_depth, f, '', ret_tuple=True)
-            print(self.prog)
-            print(agg_check_invs_at_depth)
+            agg_check_invs_at_depth = self.filter_depthmap(agg_check_invs_at_depth)
+            if len(agg_check_invs_at_depth) > 1:
+                self.print_depthplot(self.prog, agg_check_invs_at_depth, divi=1000)
+            #print(self.prog)
+            #print(agg_check_invs_at_depth)
 
         elif SEL == 21:
             agg_max_invs_at_depth = [r.max_invs_at_depth for r in rs]
