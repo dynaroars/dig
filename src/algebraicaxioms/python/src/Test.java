30e76ea3923d5fc8949278129a92f3e2bce90b1b
linhan <lli@lli.io>
2021-10-01 10:58:26 -0500

    Test.java

--- src/algebraicaxioms/python/src/Test.java
+++ src/algebraicaxioms/python/src/Test.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Test{
     public static void main(String args[]){
         System.out.println(f_0_0_0());
-System.out.println(f_0_1_0());
+System.out.println(f_1_0_0());
      }
      
         private static boolean f_0_0_0(){
@@ -16,160 +16,130 @@ System.out.println(f_0_1_0());
         }
         
         
-        //testing int_0 = pop(push(Stack_0, int_0))
+        //testing push(Stack_0, int_0)
         @SuppressWarnings("unchecked")
         private static boolean t_0_0_0_0(){
-        int int_00 = -5;
-    int []Stack_00_col = {};
+        int []Stack_00_col = {};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_01 = -5;
-    Stack_00.push(int_01);
-    int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+    int int_00 = -9;
+    NoneType push_ret0 = (NoneType)(Stack_00.push(int_00));
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_0))
+        //testing push(Stack_0, int_0)
         @SuppressWarnings("unchecked")
         private static boolean t_0_0_0_1(){
-        int int_00 = -19;
-    int []Stack_00_col = {10};
+        int []Stack_00_col = {};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_01 = -19;
-    Stack_00.push(int_01);
-    int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+    int int_00 = 32;
+    NoneType push_ret0 = (NoneType)(Stack_00.push(int_00));
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_0))
+        //testing push(Stack_0, int_0)
         @SuppressWarnings("unchecked")
         private static boolean t_0_0_0_2(){
-        int int_00 = 36;
-    int []Stack_00_col = {-31,19,20};
+        int []Stack_00_col = {};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_01 = 36;
-    Stack_00.push(int_01);
-    int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+    int int_00 = 47;
+    NoneType push_ret0 = (NoneType)(Stack_00.push(int_00));
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_0))
+        //testing push(Stack_0, int_0)
         @SuppressWarnings("unchecked")
         private static boolean t_0_0_0_3(){
-        int int_00 = -3;
-    int []Stack_00_col = {33,-24};
+        int []Stack_00_col = {};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_01 = -3;
-    Stack_00.push(int_01);
-    int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+    int int_00 = 43;
+    NoneType push_ret0 = (NoneType)(Stack_00.push(int_00));
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_0))
+        //testing push(Stack_0, int_0)
         @SuppressWarnings("unchecked")
         private static boolean t_0_0_0_4(){
-        int int_00 = 34;
-    int []Stack_00_col = {-3,-47,-2,-7,-38};
+        int []Stack_00_col = {};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_01 = 34;
-    Stack_00.push(int_01);
-    int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+    int int_00 = -4;
+    NoneType push_ret0 = (NoneType)(Stack_00.push(int_00));
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_0))
+        //testing push(Stack_0, int_0)
         @SuppressWarnings("unchecked")
         private static boolean t_0_0_0_5(){
-        int int_00 = 0;
-    int []Stack_00_col = {0,0};
+        int []Stack_00_col = {-24,39,-25};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_01 = 0;
-    Stack_00.push(int_01);
-    int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+    int int_00 = -3;
+    NoneType push_ret0 = (NoneType)(Stack_00.push(int_00));
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_0))
+        //testing push(Stack_0, int_0)
         @SuppressWarnings("unchecked")
         private static boolean t_0_0_0_6(){
-        int int_00 = -36;
-    int []Stack_00_col = {-16,49,-24,18};
+        int []Stack_00_col = {-47,48,49,27,13};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_01 = -36;
-    Stack_00.push(int_01);
-    int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+    int int_00 = 27;
+    NoneType push_ret0 = (NoneType)(Stack_00.push(int_00));
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_0))
+        //testing push(Stack_0, int_0)
         @SuppressWarnings("unchecked")
         private static boolean t_0_0_0_7(){
-        int int_00 = 11;
-    int []Stack_00_col = {30,-23,23,-43};
+        int []Stack_00_col = {-35,45,-41};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_01 = 11;
-    Stack_00.push(int_01);
-    int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+    int int_00 = -46;
+    NoneType push_ret0 = (NoneType)(Stack_00.push(int_00));
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_0))
+        //testing push(Stack_0, int_0)
         @SuppressWarnings("unchecked")
         private static boolean t_0_0_0_8(){
-        int int_00 = 19;
-    int []Stack_00_col = {32,-17};
+        int []Stack_00_col = {48,-21,3,-4};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_01 = 19;
-    Stack_00.push(int_01);
-    int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+    int int_00 = 13;
+    NoneType push_ret0 = (NoneType)(Stack_00.push(int_00));
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_0))
+        //testing push(Stack_0, int_0)
         @SuppressWarnings("unchecked")
         private static boolean t_0_0_0_9(){
-        int int_00 = -2;
-    int []Stack_00_col = {-24,11,-33,-44};
+        int []Stack_00_col = {37,-37};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_01 = -2;
-    Stack_00.push(int_01);
-    int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+    int int_00 = 25;
+    NoneType push_ret0 = (NoneType)(Stack_00.push(int_00));
+            return true;
         }
         
         
 
-        private static boolean f_0_1_0(){
+        private static boolean f_1_0_0(){
             
-            try{return (t_0_1_0_0() && t_0_1_0_1() && t_0_1_0_2() && t_0_1_0_3() && t_0_1_0_4() && t_0_1_0_5() && t_0_1_0_6() && t_0_1_0_7() && t_0_1_0_8() && t_0_1_0_9());}
+            try{return (t_1_0_0_0() && t_1_0_0_1() && t_1_0_0_2() && t_1_0_0_3() && t_1_0_0_4() && t_1_0_0_5() && t_1_0_0_6() && t_1_0_0_7() && t_1_0_0_8() && t_1_0_0_9());}
             catch(EmptyStackException e){return false;}
             catch(java.util.NoSuchElementException e){return false;}
             catch(java.lang.IndexOutOfBoundsException e){return false;}
@@ -177,153 +147,113 @@ for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
         }
         
         
-        //testing int_0 = pop(push(Stack_0, int_1))
+        //testing pop(Stack_0)
         @SuppressWarnings("unchecked")
-        private static boolean t_0_1_0_0(){
-        int int_00 = -41;
-    int []Stack_00_col = {};
+        private static boolean t_1_0_0_0(){
+        int []Stack_00_col = {};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_10 = 39;
-    Stack_00.push(int_10);
     int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_1))
+        //testing pop(Stack_0)
         @SuppressWarnings("unchecked")
-        private static boolean t_0_1_0_1(){
-        int int_00 = 17;
-    int []Stack_00_col = {-17};
+        private static boolean t_1_0_0_1(){
+        int []Stack_00_col = {-2};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_10 = -15;
-    Stack_00.push(int_10);
     int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_1))
+        //testing pop(Stack_0)
         @SuppressWarnings("unchecked")
-        private static boolean t_0_1_0_2(){
-        int int_00 = 10;
-    int []Stack_00_col = {};
+        private static boolean t_1_0_0_2(){
+        int []Stack_00_col = {-34,-24,-36,8};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_10 = -16;
-    Stack_00.push(int_10);
     int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_1))
+        //testing pop(Stack_0)
         @SuppressWarnings("unchecked")
-        private static boolean t_0_1_0_3(){
-        int int_00 = 31;
-    int []Stack_00_col = {3,19};
+        private static boolean t_1_0_0_3(){
+        int []Stack_00_col = {-13,15,-1,-39};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_10 = 40;
-    Stack_00.push(int_10);
     int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_1))
+        //testing pop(Stack_0)
         @SuppressWarnings("unchecked")
-        private static boolean t_0_1_0_4(){
-        int int_00 = 44;
-    int []Stack_00_col = {};
+        private static boolean t_1_0_0_4(){
+        int []Stack_00_col = {2};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_10 = 27;
-    Stack_00.push(int_10);
     int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_1))
+        //testing pop(Stack_0)
         @SuppressWarnings("unchecked")
-        private static boolean t_0_1_0_5(){
-        int int_00 = 39;
-    int []Stack_00_col = {-20,-31,17};
+        private static boolean t_1_0_0_5(){
+        int []Stack_00_col = {-1,-32,-11};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_10 = 11;
-    Stack_00.push(int_10);
     int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_1))
+        //testing pop(Stack_0)
         @SuppressWarnings("unchecked")
-        private static boolean t_0_1_0_6(){
-        int int_00 = 48;
-    int []Stack_00_col = {12,37,44,9,17};
+        private static boolean t_1_0_0_6(){
+        int []Stack_00_col = {32,14,35,-15};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_10 = 10;
-    Stack_00.push(int_10);
     int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_1))
+        //testing pop(Stack_0)
         @SuppressWarnings("unchecked")
-        private static boolean t_0_1_0_7(){
-        int int_00 = -13;
-    int []Stack_00_col = {24};
+        private static boolean t_1_0_0_7(){
+        int []Stack_00_col = {5,-11};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_10 = -41;
-    Stack_00.push(int_10);
     int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_1))
+        //testing pop(Stack_0)
         @SuppressWarnings("unchecked")
-        private static boolean t_0_1_0_8(){
-        int int_00 = 25;
-    int []Stack_00_col = {11,13,-31};
+        private static boolean t_1_0_0_8(){
+        int []Stack_00_col = {-21};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_10 = -6;
-    Stack_00.push(int_10);
     int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+            return true;
         }
         
 
-        //testing int_0 = pop(push(Stack_0, int_1))
+        //testing pop(Stack_0)
         @SuppressWarnings("unchecked")
-        private static boolean t_0_1_0_9(){
-        int int_00 = 19;
-    int []Stack_00_col = {-34,46,-30,-47};
+        private static boolean t_1_0_0_9(){
+        int []Stack_00_col = {};
 Stack Stack_00 = new Stack();
 for(int i = Stack_00_col.length-1; i>=0 ; --i) Stack_00.push(Stack_00_col[i]);
-    int int_10 = -49;
-    Stack_00.push(int_10);
     int pop_ret0 = (int)(Stack_00.pop());
-    boolean eq_ret0 = (boolean)(int_00 == pop_ret0);
-            return eq_ret0;
+            return true;
         }
         
         
