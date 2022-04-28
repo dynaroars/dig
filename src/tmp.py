3582e7cbc7f9f20025b3180710097d5c2dc462b6
Ubuntu <ubuntu@ip-172-31-50-251.us-west-2.compute.internal>
2022-04-28 14:57:58 +0000

    update

--- src/tmp.py
+++ src/tmp.py
@@ -8,8 +8,10 @@ x = z3.Int('x')
 y = z3.Int('y')
 f = z3.Or(z3.And(x <= -2 , y==0), x== y -1)
 
-terms = Miscs.get_terms_fixed_coefs([x,y], 2, 2)
+terms = Miscs.get_terms_fixed_coefs([x,y], 2, 1)
 
+print('input formula', f)
+print('convex hull: ')
 for t in terms:
     v,stat = SymStates.mmaximize(f, t, iupper=20)
     if v is not None:
